7f9d939b9fac7d47366cecdc0b890859f18df630
DiegoGuerrero1 <diegoguerrero@comunidad.unam.mx>
2021-10-18 16:17:49 -0500

    Interface

---
+++ Grocery.h
@@ -0,0 +1,36 @@
+/*
+ * Authors: Diego Guerrero, Luis Daiman, Ivan Olvera
+ * Contact: diegoguerrero@comunidad.unam.mx, Pongan sus correos porfas jasjas 
+ * Date: 18/10/21 
+ */ 
+
+#ifndef GROCERY_H
+#define GROCERY_H
+
+
+class Admin{
+
+	public: 
+	int[] getPrice();
+	void sell();
+
+	
+	private:
+	void addProduct();
+	void editprice();
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
