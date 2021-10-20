47aad588e3de869f6442516664b7b9e063b336c1
Luis Damián García Vega <89266189+luisdamiangv@users.noreply.github.com>
2021-10-19 21:29:41 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,39 +1,67 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+
 // #include "Grocery.h"
 
 using namespace std;
 
-
 int main() {
     int mode;
-
+    string pass;
     cout << "************ Bienvenido a Grocery 0.01 ************\n ¿Qué modo deseas utilizar? \n [1] Administrador. \n [2] Trabajador.\n Ingresa el número: " << endl;
     cin >> mode;
     switch(mode) {
         case 1:
-
+ 			cout << "Ingrese la contrasena del administrador" << endl;
+ 			cin >> pass;
+ 			while  (pass != "grocerY"){
+		cout << "Contraseña incorrecta. \n Intente de nuevo: \n" << endl;
+		cin >> pass; 
+	}
+ 			if (pass == "grocerY"){ 
+ 	
             cout << "Acciones disponibles: \n [1] Agregar Productos \n [2] Actualizar precios \n [3] Ver inventario \n [4] Venta \n Ingresa el número:" <<endl;
 	     cin >> mode;
-
 	    switch(mode){
-		    case 1:
-			    cout << "Pues ya aquí va estar implementados los métodos \n" << endl;
+		    case 1: 
+			    cout << "Agregar producto \n" << endl; //Aquí van los métodos 
 			    break;
 		    case 2:
-			    cout << "Y aquí va a estar el modo de agregar, que tomaremos de la clase Teienda \n" << endl;
-			    break;
-		    default:
-			    cout << "Esas todavía no están jeje" << endl ;
+			    cout << "Actualizar precios: \n" << endl; //Y aquí va a estar el modo de agregar, que tomaremos de la clase Tienda
+			    break; 
+			case 3:
+				cout << "Inventario: \n" << endl;
+				break;
+			case 4:
+				cout << "Venta \n" << endl;
+		    default: 
+			    cout << "Seleccione una opcion correcta\n" << endl ; 
 				    break;
-	    }
+	    } //llave final del switch de administrador
+ 
+} //llave final del if
 
-        case 2:
-	    cout << "[1] Vender producto \n[2] Agregar Productos \n Ingresa la opción: La verdad no se si esta forma sea la mejor, con muchos switchs pero si quieren pueden cambiarlo \n"<< endl;
-	    break;
+break;
+
+        case 2: 
+	    cout << "[1] Vender producto \n[2] Agregar Productos \n Ingresa la opcion: \n"<< endl;
+	    cin >> mode;
+	    switch (mode){
+	    	case 1:
+	    		cout << "Vender producto";
+	    		break;
+	    	case 2:
+	    		cout << "Agregar producto";
+	    		break;
+		}
+		break; 
+	    
+	    
         default :
-            cout << "Por el momento sólo están esas opciones disponibles :)"<< endl;
+            cout << "Por el momento solo estan esas opciones disponibles :)"<< endl;
     }
     return 0;
 
 }
-
