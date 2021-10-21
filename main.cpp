8387892684ccddde0af5b8afb44f3b9ecde29108
Luis Damián García Vega <89266189+luisdamiangv@users.noreply.github.com>
2021-10-21 16:03:04 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 /*
- * Authors: Damian Garcia, Diego Guerrero,
- * Contact: luisdamiangarcia@gmail.com , diegoguerrero@comunidad.unam.mx
+ * Authors: Damian Garcia, Diego Guerrero, Ivan Olvera
+ * Contact: luisdamiangarcia@gmail.com , diegoguerrero@comunidad.unam.mx, 
  * Date: 20/10/21
  */
+ //#include <curses.h>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,38 +11,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <curses.h>
-//#include <conio.h>
-//#include <windows.h>
+#include <conio.h>
+#include <windows.h>
 #include <iomanip>
-
 
 #include "Producto.h"
 #include "Tienda.h"
 #include "Usuario.h"
-// #include "Grocery.h"
+
 
 using namespace std;
-
-
-//FUNCIONES MENU ADMIN Y MENU TRAB, TAMBIÉN SE PUEDE PASAR A OTRO CPP PARA HACER MAS FANCY EL MAIN ;D
-int menuadmin(void){
-cout << "Modo Administrador. Acciones disponibles: \n [1] Agregar Productos \n [2] Actualizar precios \n [3] Ver inventario \n [4] Venta \n Ingresa el número:" <<endl;	
-}
-
-int menutrab(void){
-	cout << "Modo Empleado. Acciones disponibles: \n [1] Vender producto \n[2] Agregar Productos \n Ingresa la opcion: \n"<< endl;
-}
 
 int main() {
 	//Declaración de variables
     int mode, fin, afin, tfin, intento = 0, i = 0;
     string pass;
+    //objeto
     
-
-
+    
     //Inicio del programa
 do{
+	//Limpia la pantalla cada vez que empieza el ciclo
 	system("cls");
     cout << "************ Bienvenido a Grocery  ************\n ¿Que modo deseas utilizar? \n [1] Administrador. \n [2] Trabajador.\n Ingresa el numero: " << endl;
     cin >> mode; cout << "\n";
@@ -52,26 +42,30 @@ do{
  			cout << "Ingrese la clave del usuario" << endl;
  			cin >> pass; cout << "\n";
  		
- 		//Si se ingresa la contraseña equivocada 3 veces el programa te mete a modo Empleado automáticamente
+ 			//Se tienen 3 oportunidades para ingresar la contraseña correcta 
 		while  (pass != "grocerY" && intento < 2){
 		system("cls");
-		cout << "Clave incorrecta. \n Intente de nuevo: \n Quedan "<< 1 - intento + 1 << " intentos \n" << endl;
+		cout << "Clave incorrecta. \n Intente de nuevo: \n Quedan "<< 2 - intento  << " intentos \n" << endl;
 		intento++;
-		cin >> pass; }
-	
+		cin >> pass; }	
+		
  		if (pass == "grocerY"){ 
  		do {
-		 
  			//Lo primero que hace es limpiar la pantalla para mostrar el siguiente menú
  			system("cls");
-			 
- 			menuadmin();
+ 			cout << "Modo Administrador. Acciones disponibles:\n[1] Agregar Productos\n[2]Actualizar precios\n[3]Ver inventario\n[4]Venta\n Ingresa la opcion:"<<endl;
 	     	cin >> mode;
 	     	
 	    switch(mode){
 		    case 1: 
 		    system("cls");
-		    cout << "Vender producto. \n";
+		    cout << "Agregar producto. \n";
+		    cout << "Ingrese el nombre del producto";
+		   
+		 /*	Producto::setpName();
+		    Producto.nombreProductoR;
+			Product.getpName(); */
+			
 			cout << "Presiona 1 para volver al menu anterior: \n"<< endl;
 			cin >> afin;
 			break;
@@ -96,6 +90,7 @@ do{
 				cout << "Venta. \n" << endl;
 				cout << "Presiona 1 para volver al menu anterior \n"<< endl;
 				cin >> afin;
+				
 				break;
 		    
 			default: 
@@ -110,24 +105,36 @@ do{
 		break;
 
         case 2: //Modo empleado	
+	do{
+		
 		system("cls"); 
-	    menutrab();
+	    cout << "Modo Empleado. Acciones disponibles:\n[1] Agregar producto \n[2] Vender Productos \nIngresa la opcion: \n"<< endl;
 	    cin >> mode;
-		switch (mode){
+		switch (mode) { 
 	
-			
 	    	case 1:
-	    		system("cls");
-	    	cout << "Vender producto";
-	    	
+	    	system("cls");
+	    	cout << "Agregar producto\n";
+	    	 
+			//Producto::setpName("");
+			//cout << product.getId();
+		    	cout << "\t Presiona 1 para volver al menu anterior \n"<< endl;
+				cin >> tfin;
 	    	break;
 	    
 	    	case 2:
 	    	system("cls");
-	    	cout << "Agregar producto \n";
+	    	cout << "Vender producto \n";
+	    	cout << "Presiona 1 para volver al menu anterior \n"<< endl;
+				cin >> tfin;
 			
 			break;	
+			
+			default: 
+			cout << "Opcion no disponible.\n Si desea volver a intentarlo presione 1\n";
+			cin >> tfin;
 		}
+	} while (tfin=1);
 		break; 
 	    
 	    
@@ -139,6 +146,6 @@ do{
 	}
 }
 while (fin == 1);
-    return 0;
+return 0;
 
 }
